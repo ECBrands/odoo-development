409d5435b5029a7b79e4464ebad042be4ba7acc7
aktivsoftware <odoo@aktivsoftware.com>
2023-02-06 21:58:32 +0530

    [ADD] Customization module

---
+++ ec_internet_base/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "EC Internet Base",
+    "summary": "",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "",
+    "depends": ["l10n_us_check_printing"],
+    "installable": True,
+    'data': [
+        "report/print_check_bottom.xml",
+        'views/res_company_view.xml',
+    ],
+}
