0d7f10b52b3442082f97c42b9ef29231d59f779c
Isufi Kapasi <isufi.kapasi@aktivsoftware.com>
2023-03-20 18:49:46 +0530

    [ADD] #712 | ec_subscription_custom: Added module to manage note field to pass data from subscription to recurring invoice

---
+++ ec_subscription_custom/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, ECInternets, Aktiv Software.
+# See LICENSE file for full copyright & licensing details.
+
+# Author: Aktiv Software PVT. LTD.
+# mail: odoo@aktivsoftware.com
+# Copyright (C) 2015-Present Aktiv Software PVT. LTD.
+# Contributions:
+#   Aktiv Software:
+#     - Isufi Kapasi
+
+{
+    'name': 'EC Subscription Customs',
+    'version': '16.0.1.0.0',
+    'sequence': 12,
+    'summary': 'EC Subscription Customs',
+    'category': 'Sales',
+    'website': '',
+    'depends': [
+        'account_move_customs',
+        'sale_subscription'
+    ],
+    'data': [
+        'views/sale_subscription_views.xml'
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
