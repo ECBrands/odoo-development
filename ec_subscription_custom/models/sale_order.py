c6adca4faabd7740c9ef739c93d5ab883e45c2d0
Isufi Kapasi <isufi.kapasi@aktivsoftware.com>
2023-03-20 18:49:46 +0530

    [ADD] #712 | ec_subscription_custom: Added module to manage note field to pass data from subscription to recurring invoice

---
+++ ec_subscription_custom/models/sale_order.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, ECInternets, Aktiv Software.
+# See LICENSE file for full copyright & licensing details.
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    internal_note = fields.Html(string="Internal Note")
+
+    def _prepare_invoice(self):
+        """Override: Prepare Invoice Lines
+
+        To Pass the Order Note to Invoice.
+
+       :return: Super Call with updated res.
+       """
+        res = super(SaleOrder, self)._prepare_invoice()
+        res['internal_note'] = self.internal_note
+        return res
